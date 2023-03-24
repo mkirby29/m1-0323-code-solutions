cf558d4a98210a360e62cda251f0a87dba4b6c10
Matt Kirby <MattKir29@gmail.com>
2023-03-24 02:45:43 +0000

    completed javascript-data-model exercise

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,73 @@
+const orderHistory = [
+  {
+    placed: 'August 4, 2020',
+    total: '$34.00',
+    shipTo: 'JS Masher',
+    orderNo: '114-3941689-8772232',
+    delivered: 'Delivered Aug 8, 2020',
+    received: ' ',
+    items: [
+      {
+        title: 'JavaScript for impatient programmers',
+        author: 'Rauschmayer, Dr. Axel',
+        return: 'Return window closed on Sep 7, 2020',
+        price: '$31.55'
+      }
+    ]
+  },
+  {
+    placed: 'July 19, 2020',
+    total: '$44.53',
+    shipTo: 'JS Masher',
+    orderNo: '113-9984268-1280257',
+    delivered: 'Delivered Jul 20, 2020',
+    received: 'Your package was delivered. It was handed directly to a resident.',
+    items: [
+      {
+        title: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        return: 'Return window closed on Aug 19, 2020',
+        price: '$41.33'
+      }
+    ]
+  },
+  {
+    placed: 'July 4, 2020',
+    total: '$17.22',
+    shipTo: 'JS Masher',
+    orderNo: '114-2875557-9059409',
+    delivered: 'Delivered Jul 7, 2020',
+    received: 'Your package was delivered. It was handed directly to a resident.',
+    items: [
+      {
+        title: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+        author: ' ',
+        return: 'Return window closed on Aug 5, 2020',
+        price: '$15.98'
+      }
+    ]
+  },
+  {
+    placed: 'July 3, 2020',
+    total: '$138.93',
+    shipTo: 'JS Masher',
+    orderNo: '113-2883177-2648248',
+    delivered: 'Delivered Jul 5, 2020',
+    received: ' ',
+    items: [
+      {
+        title: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        author: ' ',
+        return: 'Return window closed on Aug 4, 2020',
+        price: '$94.95'
+      },
+      {
+        title: 'The Art of Sql',
+        author: 'Faroult, Stephane',
+        return: 'Return window closed on Aug 4, 2020',
+        price: '$33.99'
+      }
+    ]
+  }
+];
+console.log(orderHistory);
