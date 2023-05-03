a60c4d06ae2b7021a61973de8de5ead7c991815f
Matt Kirby <MattKir29@gmail.com>
2023-05-03 03:37:57 +0000

    completed dom-querying exercise

---
+++ dom-querying/main.js
@@ -0,0 +1,19 @@
+console.log('hello,world');
+
+const element = document.querySelector('h1');
+console.log(element);
+console.dir(element);
+
+const attr = document.querySelector('#explanation');
+console.log(attr);
+console.dir(attr);
+
+const guess = document.querySelector('.hint');
+console.log(guess);
+console.dir(guess);
+
+const list = document.querySelectorAll('p');
+console.log(list);
+
+const link = document.querySelectorAll('.example-link');
+console.log(link);
